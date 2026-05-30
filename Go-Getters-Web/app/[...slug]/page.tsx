26e625918bb6534718185f3151d834f866c3bacf
Shollyjay <akintayojoseph64@gmail.com>
2026-05-31 00:09:53 +0100

    fix(web): migrate optional catch-all to clean root page.tsx and sub-path catch-all [...slug]/page.tsx to resolve Vercel routing 404s

---
+++ Go-Getters-Web/app/[...slug]/page.tsx
@@ -0,0 +1,9 @@
+import dynamic from 'next/dynamic';
+
+export const dynamic = "force-dynamic";
+
+const AppSPA = dynamic(() => import('../AppSPA'), { ssr: false });
+
+export default function Page() {
+  return <AppSPA />;
+}
