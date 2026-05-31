0a253e8644d93fe048fcf6daf26194b1c8ad9565
Shollyjay <akintayojoseph64@gmail.com>
2026-05-31 13:13:19 +0100

    fix(web): add Webpack DefinePlugin to mock __dirname globally as a string to resolve Edge Runtime ReferenceError crash

--- Go-Getters-Web/next.config.js
+++ Go-Getters-Web/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { webpack, isServer }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __dirname: '"/"',
+      })
+    );
+    return config;
+  },
 };
 
 module.exports = nextConfig;
